a404bcb023f1f11fd46813860388e1b3d12b43a3
mrchnk <mrchnk@gmail.com>
2019-01-17 13:10:53 +0300

    implement basic auth

--- encode.js
+++ encode.js
@@ -1,8 +1,8 @@
 function encode(name, parameters = {}) {
-    const body = Object.keys(parameters)
-        .map(key => `<${key}>${parameters[key]}</${key}`)
-        .join('');
-    return `<?xml version="1.0" encoding="utf-8"?><${name}>${body}</${name}>`;
+  const body = Object.keys(parameters)
+    .map(key => `<${key}>${parameters[key]}</${key}>`)
+    .join('');
+  return `<?xml version="1.0" encoding="utf-8"?><${name}>${body}</${name}>`;
 }
 
 module.exports = encode;
--- index.js
+++ index.js
@@ -1,53 +1,122 @@
 const axios = require('axios');
 const {parseString} = require('xml2js');
 
-const _encode = require('./encode');
 const _endpoints = Object.freeze({
-    auth: '/roap/api/auth',
-    command: '/roap/api/command',
+  auth: '/roap/api/auth',
+  command: '/roap/api/command',
+  data: '/roap/api/data',
+  event: '/roap/api/event',
+  navigation: '/navigation',
 });
 
-function LGTV(parameters = {}) {
-    const
-        { host, port = 8080 } = parameters,
-        encode = parameters.encode || _encode,
-        endpoints = parameters.endpoints || _endpoints;
+const _encode = require('./encode');
 
-    function send(path = "/", data = '') {
-        return axios({
-            method: 'post',
-            url: `http://${host}:${port}${path}`,
-            response: 'string',
-            headers: {
-                'Content-Type': "application/atom+xml",
-                'Connection': "Keep-Alive"
-            },
-            data
-        })
-        .then((response) => 
-            new Promise((resolve, reject) =>
-                parseString(response.data, (err, result) => {
-                    if (err) reject(err);
-                    else resolve(result);
-                })
-            ))
-        .catch(error => { throw error } );
-    }
+const _decode = (data) => new Promise((resolve, reject) => {
+  parseString(data, {
+    explicitRoot: false,
+    explicitArray: false,
+  }, (err, result) => {
+    if (err) reject(err);
+    else resolve(result);
+  })
+})
 
-    return {
-        send,
-        AuthKeyReq : () => send(endpoints.auth, encode('auth', {
-            type: 'AuthKeyReq',
+/**
+ * @typedef LGTVSettings
+ * @property {string} host ip address or hostname of TV
+ * @property {integer} [port] port (default is 8080)
+ * @property {function} [encode]
+ * @property {endpoints} [encode]
+ * @property {string} [pairingKey]
+ * 
+ * @param {LGTVSettings} settings 
+ */
+function LGTV(settings = {}) {
+  const
+    { host, port = 8080 } = settings,
+    encode = settings.encode || _encode,
+    endpoints = settings.endpoints || _endpoints;
+  
+  function post(path = "/", data = '') {
+    return axios({
+      method: 'post',
+      url: `http://${host}:${port}${path}`,
+      response: 'string',
+      headers: {
+        'Content-Type': "application/atom+xml",
+        'Connection': "Keep-Alive"
+      },
+      data
+    })
+    .then((request) => _decode(request.data))
+  }
+
+  function get(path = "/") {
+    return axios({
+      method: 'get',
+      url: `http://${host}:${port}${path}`,
+      response: 'string',
+      headers: {
+        'Connection': "Close"
+      }
+    })
+  }
+
+  return {
+    get,
+    post,
+
+    auth: {
+
+      AuthKeyReq: () => post(endpoints.auth, encode('auth', {
+        type: 'AuthKeyReq',
+      })),
+
+      CancelAuthKeyReq: () => post(endpoints.auth, encode('auth', {
+        type: 'CancelAuthKeyReq'
+      })),
+
+      AuthReq: (pairingKey) => post(endpoints.auth, encode('auth', {
+        type: 'AuthReq',
+        value: pairingKey || settings.pairingKey
+      })),
+    },
+
+    command: {
+      
+      /**
+       * Not working
+       */
+      ChangeInputSource: (source) => 
+        typeof source == "string" ?
+          post(endpoints.command, encode('command', {
+            name: 'ChangeInputSource',
+            inputSource: source,
+          })) :
+          post(endpoints.command, encode('command', {
+            name: 'ChangeInputSource',
+            inputSourceType: source.type,
+            inputSourceIdx: source.index
+          })),
+
+      HandleKeyInput: (key) =>
+        post(endpoints.command, encode('command', {
+          name: 'HandleKeyInput',
+          value: key
         })),
-        AuthReq: (pairingKey) => send(endpoints.auth, encode('auth', {
-            type: 'AuthReq',
-            value: pairingKey || parameters.pairingKey
-        })),
+
+    },
+
+    data: {
+
+      inputsrc_list: () => get(`${endpoints.data}?target=inputsrc_list`)
+
     }
+  }
 };
 
-LGTV.encode = _encode;
 LGTV.endpoints = _endpoints;
+LGTV.encode = _encode;
+LGTV.decode = _decode;
 
 module.exports = LGTV;
-
